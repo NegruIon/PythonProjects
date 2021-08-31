9a96ca085722dfbfa861bd3829fe9cab64c134bd
Ion Vrinceanu <vrinceanuion@gmail.com>
2021-08-31 19:00:44 +0300

    fix non-ascii char error

--- File_Show/file_show.py
+++ File_Show/file_show.py
@@ -16,12 +16,12 @@ def choose_file(dir_path, number_of_lines, is_folder_var):
     for name in os.listdir(dir_path):
         if is_folder_var:
             if os.listdir(dir_path)[-1] == name:
-                print(("  |" * (number_of_lines-1)) + "  ﹂" + name)
+                print(("  |" * (number_of_lines-1)) + " -" + name)
             else:
                 print(("  |" * (number_of_lines-1)) + "  |-" + name)
         else:
             if os.listdir(dir_path)[-1] == name:
-                print("  ﹂" + name)
+                print("  -" + name)
             else:
                 print("  |-" + name)
         if os.path.isdir(dir_path+"\\"+name):
