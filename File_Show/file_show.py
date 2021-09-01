7ebc6c10d40e4673b5b35c197c63bc9d6f32d90f
NegruIon <ionegru776@gmail.com>
2021-09-01 18:12:40 +0300

    Added possibility for users to enter multiple paths or exit

--- File_Show/file_show.py
+++ File_Show/file_show.py
@@ -1,32 +1,14 @@
-import os
+from functions import choose_file, os
 
 
-separator = os.sep
-
-path = input("Enter the path to be scanned: ")
-
-# New Comment
 number_of_lines = 0
 is_folder = False
 
 
-def choose_file(dir_path, number_of_lines, is_folder_var):
-    number_of_lines += 1
-    for name in os.listdir(dir_path):
-        if is_folder_var:
-            if os.listdir(dir_path)[-1] == name:
-                print(("  |" * (number_of_lines-1)) + " -" + name)
-            else:
-                print(("  |" * (number_of_lines-1)) + "  |-" + name)
-        else:
-            if os.listdir(dir_path)[-1] == name:
-                print("  -" + name)
-            else:
-                print("  |-" + name)
-        if os.path.isdir(dir_path+separator+name):
-            is_folder = True
-            choose_file(dir_path+separator+name, number_of_lines, is_folder)
-
-
-print(os.path.basename(path))
-choose_file(path, number_of_lines, is_folder)
+while True:
+    path = input("Enter the path to be scanned or quit: ")
+    if path == "quit":
+        break
+    else:
+        print(os.path.basename(path))
+        choose_file(path, number_of_lines, is_folder)
